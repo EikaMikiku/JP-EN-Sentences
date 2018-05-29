804df6fa34a96b43f2fc5b7d1b137cc7bfdd8ab4
EikaMikiku <e.i.k.a@outlook.com>
2018-05-29 11:51:38 +0100

    Added speaking training

--- learn.js
+++ learn.js
@@ -6,6 +6,7 @@ function Learn() {
 	this.engList = document.getElementById("english-list");
 	this.sentenceList = document.getElementById("sentence-list");
 	this.history = document.getElementById("history");
+	this.understand = document.getElementById("understand-speak");
 	this.jsLocation = "./extracted/";
 	this.choiceCount = 6;
 
@@ -16,12 +17,90 @@ function Learn() {
 	this.previousCorrect = null;
 }
 
-Learn.prototype.populateTrainDom = function(pickedItems, correct) {
-	this.previousCorrect = pickedItems[correct].item.id;
+Learn.prototype.populateTrainSpeakingDom = function(pickedItems, correct) {
+	//English card
+	let engText = this.getEngText(pickedItems[correct]);
+	let engDiv = document.createElement("div");
+	engDiv.innerText = engText;
+	this.jpWord.appendChild(engDiv);
+	this.jpWord.style.fontSize = "3vw";
+	let jpAudio = this.getAudio(pickedItems[correct].sound);
+	this.jpWord.onclick = () => {
+		jpAudio.play();
+	};
 
+	//Japanese romaji cards
+	for(let i = 0; i < pickedItems.length; i++) {
+		let div = document.createElement("div");
+		div.className = "card eng selectable"; //Re-using same classes
+		div.innerText = this.getRomajiText(pickedItems[i]);
+
+		div.onclick = (e) => {
+			if(e.target.classList.contains("correct")) {
+				this.clean();
+				this.show();
+				return;
+			}
+			let isCorrect = i === correct;
+			e.target.classList.add(isCorrect ? "correct" : "incorrect");
+			let audio = this.getAudio(pickedItems[i].sound);
+			audio.play();
+
+			if(isCorrect) {
+				//Show sentences on correct selection
+				let sentences = pickedItems[i].sentences;
+				for(let k = 0; k < sentences.length; k++) {
+					let sentenceAudio = this.getAudio(sentences[k].sound);
+					let sDiv = document.createElement("div");
+					sDiv.className = "col-6"; //Half screen
+					let sCard = document.createElement("div");
+					sCard.className = "card selectable";
+					sDiv.appendChild(sCard);
+					
+					let engDiv = document.createElement("div");
+					engDiv.innerHTML = this.getEngSentenceText(sentences[k]);
+
+					let jpNoKanjiDiv = document.createElement("div");
+					let jpKanjiDiv = document.createElement("div");
+					let noKanjiHtml = this.getJpSentenceText(sentences[k]);
+					let kanjiHtml = sentences[k].cue.text;
+					jpNoKanjiDiv.innerHTML = noKanjiHtml;
+					jpKanjiDiv.innerHTML = kanjiHtml;
+
+					if(kanjiHtml.replace(/\s/g, "") !== noKanjiHtml.replace(/\s/g, "")) {
+						sCard.appendChild(jpNoKanjiDiv);
+					}
+					sCard.appendChild(jpKanjiDiv);
+					sCard.appendChild(engDiv);
+
+					sCard.onclick = (e) => {
+						sentenceAudio.play();
+					};
+
+					this.sentenceList.appendChild(sDiv);
+				}
+			}
+
+			let isFirstAnswer = document.querySelectorAll(".incorrect,.correct").length === 1;
+			if(isFirstAnswer) {
+				//Update history
+				this.historyTotal++;
+				if(isCorrect) {
+					this.historyCorrect++;
+				}
+				this.history.innerText = this.historyCorrect + "/" + this.historyTotal;
+			}
+		};
+
+		this.engList.appendChild(div);
+	}
+}
+
+Learn.prototype.populateTrainUnderstandingDom = function(pickedItems, correct) {
 	//Japanese card
 	let jpNoKanji = this.getJpText(pickedItems[correct]);
 	let jpKanji = pickedItems[correct].item.cue.text;
+
 
 	let jpKanjiDiv = document.createElement("div");
 	jpKanjiDiv.innerText = jpKanji;
@@ -55,12 +134,12 @@ Learn.prototype.populateTrainDom = function(pickedItems, correct) {
 			let isCorrect = i === correct;
 			e.target.classList.add(isCorrect ? "correct" : "incorrect");
 			let audio = this.getAudio(pickedItems[i].sound);
+			
 			if(!isCorrect) {
 				audio.play();
 			} else {
-				//Show english translation on sentenses
-				let el = document.getElementsByClassName("english-sentense");
-				console.log(el);
+				//Show english translation on sentences
+				let el = document.getElementsByClassName("english-sentence");
 				for(let k = 0, len = el.length; k < len; k++) {
 					el[k].style.display = "block";
 				}
@@ -90,7 +169,7 @@ Learn.prototype.populateTrainDom = function(pickedItems, correct) {
 		sDiv.appendChild(sCard);
 		
 		let engDiv = document.createElement("div");
-		engDiv.className = "english-sentense";
+		engDiv.className = "english-sentence";
 		engDiv.innerHTML = this.getEngSentenceText(sentences[k]);
 
 		let jpNoKanjiDiv = document.createElement("div");
@@ -158,12 +237,21 @@ Learn.prototype.getJpText = function(word) {
 	}
 };
 
+Learn.prototype.getRomajiText = function(word) {
+	return word.item.cue.transliterations.Latn || "???";
+};
+
 Learn.prototype.show = function() {
 	this.jpWord.style.display = "flex";
 	this.engList.style.display = "block";
 	let pickedItems = this.pick(this.choiceCount);
 	let correct = Math.floor(Math.random() * this.choiceCount);
-	this.populateTrainDom(pickedItems, correct);
+	this.previousCorrect = pickedItems[correct].item.id;
+	if(this.understand.checked) {
+		this.populateTrainSpeakingDom(pickedItems, correct);
+	} else {
+		this.populateTrainUnderstandingDom(pickedItems, correct);
+	}
 };
 
 Learn.prototype.pick = function(amount) {
